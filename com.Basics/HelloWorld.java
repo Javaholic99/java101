cbf1a48cc6a5abefe749d2a50649a521e70bda87
ashfaq26 <ashfaq.adeni@excellarate.com>
2023-04-14 12:58:21 +0530

    initial commits of old files

---
+++ com.Basics/HelloWorld.java
@@ -0,0 +1,20 @@
+package com.java101.basics;
+
+// Class is a container  which has code in it Main method is the starting point from where your java code starts executing
+// Class Names follow PascalNamingConvention
+//Method Names follow camelNamingConvention
+// All java files have .java extension
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
+/**
+ * The System class and PrintStream class belong to the Java standard library.
+ * Java.lang System: This is a class in Java that provides access to the system,
+ * including standard input, output, and error streams. out: This is a static
+ * field of the System class that provides access to the standard output stream.
+ * println(): This is a method of the PrintStream class, which is the type of
+ * the out field. This method prints the specified string or value to the
+ * console followed by a new line.
+ */
