86481dc437c79e1b4c53ad8f6722352cbc7d49d3
ashfaq26 <ashfaq.adeni@excellarate.com>
2023-06-01 06:58:56 +0530

    removing duplicate values from a string

---
+++ com.Basics/com.strings/StringDuplicate.java
@@ -0,0 +1,21 @@
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class StringDuplicate {
+    public static void main(String[] args) {
+        String myString = "ashfaqqad";
+        char a[]= myString.toCharArray();
+        Set <Character> mySet=new LinkedHashSet();
+        for (char c : a) {
+            mySet.add(c);
+        }
+        System.out.println(mySet);
+        StringBuilder sb= new StringBuilder();
+        for (Character character : mySet) {
+            sb.append(character);
+        }
+        System.out.println(sb.toString());
+    }
+}
